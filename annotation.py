512815bf59351e55be434671c0bed36ee1950de3
jdpeiffer6 <j.d@wustl.edu>
2024-04-01 23:45:04 -0500

    Changed annotation

--- annotation.py
+++ annotation.py
@@ -1,18 +1,24 @@
-from glob import glob
-import pandas as pd
 import streamlit as st
-from streamlit_image_annotation import classification
+from glob import glob
+from streamlit_image_annotation import pointdet
 
 label_list = ['deer', 'human', 'dog', 'penguin', 'framingo', 'teddy bear']
 image_path_list = glob('*.png')
-if 'result_df' not in st.session_state:
-    st.session_state['result_df'] = pd.DataFrame.from_dict({'image': image_path_list, 'label': [0]*len(image_path_list)}).copy()
+if 'result_dict' not in st.session_state:
+    result_dict = {}
+    for img in image_path_list:
+        result_dict[img] = {'points': [],'labels':[]}
+    st.session_state['result_dict'] = result_dict.copy()
 
-num_page = st.slider('page', 0, len(image_path_list)-1, 0)
-label = classification(image_path_list[num_page], 
+
+num_page = st.slider('page', 0, len(image_path_list)-1, 0, key='slider')
+target_image_path = image_path_list[num_page]
+
+new_labels = pointdet(image_path=target_image_path, 
                         label_list=label_list, 
-                        default_label_index=int(st.session_state['result_df'].loc[num_page, 'label']))
-
-if label is not None and label['label'] != st.session_state['result_df'].loc[num_page, 'label']:
-    st.session_state['result_df'].loc[num_page, 'label'] = label_list.index(label['label'])
-st.table(st.session_state['result_df'])
+                        points=st.session_state['result_dict'][target_image_path]['points'],
+                        labels=st.session_state['result_dict'][target_image_path]['labels'], key=target_image_path)
+if new_labels is not None:
+    st.session_state['result_dict'][target_image_path]['points'] = [v['point'] for v in new_labels]
+    st.session_state['result_dict'][target_image_path]['labels'] = [v['label_id'] for v in new_labels]
+st.json(st.session_state['result_dict'])
