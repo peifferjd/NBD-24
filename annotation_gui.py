e8cc623d4bd922bd9c46b5a97429d522ef164588
jdpeiffer6 <johnpeiffer2026@u.northwestern.edu>
2024-04-02 18:01:43 -0500

    GUI works

--- annotation_gui.py
+++ annotation_gui.py
@@ -1,23 +1,10 @@
 import streamlit as st
-from PIL import Image as pImage
-import tempfile
-import numpy as np
-import os
-
-
-
-
-
-import streamlit as st
 import numpy as np
 from annotation_schema import CroppedImage
 from streamlit_image_coordinates import streamlit_image_coordinates
-import os
-
-if 'points' not in st.session_state:
-    st.session_state['points'] = []
-    
 from PIL import Image, ImageDraw
+
+
 def get_ellipse_coords(point: tuple[int, int]) -> tuple[int, int, int, int]:
     center = point
     radius = 10
@@ -28,21 +15,45 @@ def get_ellipse_coords(point: tuple[int, int]) -> tuple[int, int, int, int]:
         center[1] + radius,
     )
 
-key = np.random.choice(CroppedImage.fetch("KEY"))
-array = (CroppedImage & key).fetch1("image_cropped")
-with Image.fromarray(array) as img:
+ ### WORKS GOOD FOR TWO POINTS
+from PIL import Image, ImageDraw
+import streamlit as st
+
+st.write(st.secrets['datajoint'])
+
+## CLOSE BUT KEEPS LAST POINT
+# # List of filenames to choose from
+keys = CroppedImage.fetch('KEY')
+
+if "points" not in st.session_state:
+    st.session_state["points"] = []
+if "key" not in st.session_state:
+    st.session_state["key"] = keys[0]
+if "new_image_selected" not in st.session_state:
+    st.session_state["new_image_selected"] = False  # Flag to indicate a new image was selected
+
+# Submit button to choose a random filename and clear points
+if st.button('Choose Random Image'):
+    st.session_state["key"] = np.random.choice(keys)
+    st.session_state["points"].clear()
+    st.session_state["new_image_selected"] = True  # Set the flag to True
+
+with Image.fromarray((CroppedImage & st.session_state['key']).fetch1('image_cropped')) as img:
     draw = ImageDraw.Draw(img)
 
-    # Draw an ellipse at each coordinate in points
-    for point in st.session_state["points"]:
+    # Draw an ellipse at each coordinate in the last two points
+    for point in st.session_state["points"][-2:]:
         coords = get_ellipse_coords(point)
         draw.ellipse(coords, fill="red")
 
     value = streamlit_image_coordinates(img, key="pil")
 
-    if value is not None:
-        point = value["x"], value["y"]
-
+    if value is not None and not st.session_state["new_image_selected"]:  # Check the flag before adding a new point
+        point = (value["x"], value["y"])
         if point not in st.session_state["points"]:
+            if len(st.session_state["points"]) >= 2:
+                st.session_state["points"].pop(0)
             st.session_state["points"].append(point)
             st.experimental_rerun()
+
+    st.session_state["new_image_selected"] = False 
