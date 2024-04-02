394db142acfe00e0c355b8044da8270ecc48e343
jdpeiffer6 <j.d@wustl.edu>
2024-04-02 01:03:00 -0500

    Changes to gui

--- annotation_gui.py
+++ annotation_gui.py
@@ -1,16 +1,22 @@
 import streamlit as st
 from streamlit_image_annotation import pointdet
 from annotation_schema import Image, Label
+from PIL import Image as pImage
+import tempfile
 import numpy as np
+import os
 
 
-
-fname = np.random.choice(Image.fetch('fname'))
+key = np.random.choice(Image.fetch('KEY'))
+_, array = (Image & key).fetch1('fname', 'image')
+# save array as tmpfile
+fname = tempfile.mktemp(suffix='.png')
+pImage.fromarray(array).save(fname)
 
 label_list = ['eye','na']
 result_dict = {'points':[[0,0]],'labels':[1]}
 
-new_labels = pointdet(image_path='faces_unannotated/'+fname, 
+new_labels = pointdet(image_path=fname, 
                         label_list=label_list, 
                         points=result_dict['points'],
                         labels=result_dict['labels'],
@@ -19,4 +25,6 @@ if new_labels is not None:
     result_dict['points'] = [v['point'] for v in new_labels]
     result_dict['labels'] = [v['label_id'] for v in new_labels]
     st.json(result_dict)
+    # cleanup temp
+    os.remove(fname)
     st.rerun()
